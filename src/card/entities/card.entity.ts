d1babde7e2b3e91511cef97563561ad9b351d057
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-13 15:45:01 +0530

    created card entity

--- src/card/entities/card.entity.ts
+++ src/card/entities/card.entity.ts
@@ -1,1 +1,59 @@
-export class Card {}
+import { PrimaryGeneratedColumn, Column, Entity, CreateDateColumn, ManyToOne } from "typeorm";
+import { Account } from "../../account/entities/account.entity";
+
+
+export enum CardType {
+    DEBIT = 'DEBIT',
+    CREDIT = 'CREDIT'
+}
+
+export enum CardStatus {
+    ACTIVE = "ACTIVE",
+    BLOCKED = "BLOCKED",
+    EXPIRED = "EXPIRED"
+}
+
+
+@Entity()
+export class Card {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    cardNumber: string;
+
+    @Column({
+        type: 'enum',
+        enum: CardType,
+    })
+    cardType: CardType;
+
+    @Column({
+        type: 'enum',
+        enum: CardStatus,
+    })
+    status: CardStatus;
+
+    @Column({
+        length: 4
+    })
+    cvv: string;
+
+
+    @Column({
+        nullable: true
+    })
+    pin: string;
+
+    @Column()
+    expiryDate: Date;
+
+    @CreateDateColumn()
+    createdAt: Date
+
+
+    @ManyToOne(() => Account, (account) => account.cards)
+    account: Account;
+
+}
