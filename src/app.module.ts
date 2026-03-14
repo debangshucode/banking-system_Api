80aa411e131bbac7f1227cc46df843ce4a19d57f
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-14 16:08:18 +0530

    Cookie session || current user middleware || signout

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -9,7 +9,9 @@ import { CardModule } from './card/card.module';
 import { FixedDepositModule } from './fixed-deposit/fixed-deposit.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
+import { ConfigService } from '@nestjs/config';
+import cookieSession from 'cookie-session';
+import { CurrentUserMiddleware } from './users/middleware/current-user.middleware';
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
@@ -30,4 +32,17 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+
+  constructor(private configService:ConfigService){}
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(
+      cookieSession({
+        keys:[this.configService.get('COOKIE_KEY')!]
+      }),
+      CurrentUserMiddleware
+    ).forRoutes('*')
+  }
+  
+
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Session, HttpCode } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Serialize } from '../interceptor/serialize.interceptor';
 import { UserDto } from './dto/user.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './entities/user.entity';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  // *currentUser
+  @Get('/curUser')
+  @Serialize(UserDto)
+  async getCurUSer(@CurrentUser() user:User){
+    return user
+  }
   
   // * create user/signup
   @Post()
   @Serialize(UserDto)
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto, @Session() session:any) {
+    const user = await this.usersService.create(createUserDto);
+    session.userId = user.id;
+    return user;
   }
 
   // * signin
   @Post('/signin')
   @Serialize(UserDto)
-  signIn(@Body() createUserDto:CreateUserDto) {
-    return this.usersService.signIn(createUserDto.email,createUserDto.password)
+  async signIn(@Body() createUserDto:CreateUserDto, @Session() session:any) {
+    const user = await this.usersService.signIn(createUserDto.email,createUserDto.password)
+    session.userId = user.id;
+    return user;
+  }
+
+  // * signOut
+  @Post('/signout')
+  @HttpCode(200)
+  async signout(@Session() session:any){
+    session.userId = null;
   }
 
   @Get()
