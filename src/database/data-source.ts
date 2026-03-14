92124513f84e1528c355f2296eeed0f55251776a
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-14 10:42:29 +0530

    complete migration with pgsql

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -7,8 +7,15 @@ dotenv.config();
 const dbName = process.env.DB_NAME;
 
 export const AppDataSource = new DataSource({
-    type: 'sqlite',
-    database: process.env.DB_NAME || 'bank.sqlite',
+    type: 'postgres',
+    host: process.env.DB_HOST || (() => { throw new Error('DB_HOST is required'); })(),
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME || (() => { throw new Error('DB_USERNAME is required'); })(),
+    password: process.env.DB_PASSWORD || (() => { throw new Error('DB_PASSWORD is required'); })(),
+    database: dbName,
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // }, no need for local
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
 });
