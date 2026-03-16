368dbd704265813267ce8720fd3a6655dadc048c
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-16 18:12:40 +0530

    railway deploy fix - Railway needs the server to listen on 0.0.0.0.

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const documnetFactory = ()=> SwaggerModule.createDocument(app,config);
   SwaggerModule.setup('api',app,documnetFactory)
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
