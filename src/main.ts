682c586f034f51e4bfa3eef4a74e216d50bf2d4c
Debangshu Dey <deydebangshu51@gmail.com>
2026-03-16 18:33:05 +0530

    fix the port for railway

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Banking-System')
-  .setDescription('This is a Banking System api Documentation')
-  .setVersion('1.0')
-  .addTag('Bank')
-  .build()
+    .setTitle('Banking-System')
+    .setDescription('This is a Banking System api Documentation')
+    .setVersion('1.0')
+    .addTag('Bank')
+    .build();
 
-  const documnetFactory = ()=> SwaggerModule.createDocument(app,config);
-  SwaggerModule.setup('api',app,documnetFactory)
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  const port = Number(process.env.PORT) || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Server running on port ${port}`);
 }
+
 bootstrap();
